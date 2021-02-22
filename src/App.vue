35bbf8653ffffb32637c3bb39ee6964edf8745c9
laizp <1550377556@qq.com>
2021-02-23 00:51:30 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <router-link to="/">Home</router-link>
+      <router-link v-for="i in 10" :key="i" :to="`/page/${i}`" class="link">{{ i }}</router-link>
+    </div>
+
+    <router-view
+      v-slot="{ Component }"
+      class="view"
+    >
+      <Suspense>
+        <component :is="Component" />
+      </Suspense>
+    </router-view>
+  </div>
+</template>
+
+<script setup>
+
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+.nav {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+.view {
+  margin-top: 20px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/page.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page">
+    page {{ id }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+import { onBeforeRouteUpdate, useRoute } from 'vue-router';
+
+export default {
+  name: 'Page',
+  async setup() {
+    const route = useRoute();
+    const id = ref(route.params.id);
+
+    onBeforeRouteUpdate((to) => {
+      id.value = to.params.id;
+      console.log(id.value);
+    });
+
+    return {
+      id
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
